a5ad5585b09a8598963a9dffce97db432b0af579
ramesh06190 <apvs.ramesh@gmail.com>
2022-10-26 23:38:00 -0500

    Modified Compute end point

--- routes/computation.js
+++ routes/computation.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var x=Math.floor(Math.random()*5);
+
 
 /* GET Computation page. */
 router.get('/', function(req, res, next) {
+  var x=Math.floor(Math.random() * 100);
   if(req.query.x!= null && req.query.x!=undefined) {
     x= req.query.x
   }
-  var y=Math.abs(x);
-  res.render('computation', { title: 'Computing cube root of a number',content:`Math.cbrt() applied to ${y} is ${Math.cbrt(y)}`, content1:`\nMath.atan2() applied to ${y} is ${Math.atan2(y)}`,
-  content3:`\nMath.atanh() applied to ${y} is ${Math.atanh(y)}`});
+  res.render('computation', { title: 'Computing cube root of a number',content:`Math.cbrt() applied to ${x} is ${Math.cbrt(x)}`});
 });
 
 module.exports = router;
+
