ecd1a2567033e5e9a380194eeccd141dde8edcb9
ramesh06190 <apvs.ramesh@gmail.com>
2022-10-26 22:30:37 -0500

    modified computation view

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var x=Math.random();
+var x=Math.floor(Math.random()*5);
 
-/* GET my page. */
+/* GET Computation page. */
 router.get('/', function(req, res, next) {
+  if(req.query.x!= null && req.query.x!=undefined) {
+    x= req.query.x
+  }
+  var y=Math.abs(x);
+  var y1=Math.cbrt(x);
+  var y2=Math.atan2(x);
+  var y3=Math.atanh(x);
   res.render('computation', { title: 'Computing cube root of a number',content:`Math.cbrt() applied to ${x} is ${Math.cbrt(x)}`});
 });
 
